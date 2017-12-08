0025b22aed3e8c95f5062ea8aafcea062ab7063d
binamkayastha <binamkayastha@gmail.com>
2017-12-08 09:14:27 -0500

    Add ruby db intialization, and some basic way of example of adding something to the db.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'pages/registration'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'submitData' => 'pages#submitData', as: :submitData
 end
